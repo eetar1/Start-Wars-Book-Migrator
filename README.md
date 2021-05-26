7d990b31bc826978d8e5fb16a5363943030f67bd
eetar1 <ekwan14@gmail.com>
2021-05-26 14:10:13 -0700

    Create Migrator

---
+++ README.md
@@ -0,0 +1,16 @@
+#Star Wars Legends Book Migrator
+
+This program will pull the list of stars books from the legends timeline and then 
+insert them in to a local mysql db
+
+##Mysql DB Schema
+The price and owned columns are not required 
+![img.png](img.png)
+
+## Running the program
+This program requires beautiful soup and python requests
+
+### To run
+```shell
+python3 migrator.py
+```
---
+++ migrator.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+import mysql.connector
+
+
+def defGetPage():
+    resp = requests.get('https://starwars.fandom.com/wiki/Timeline_of_Legends_books')
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    table = soup.findAll("table")[4]
+    rows = list()
+
+    # Initial inits these vars sometimes have to be used from previous rows
+    author = ''
+    timeline = ''
+    title = ''
+    released = ''
+
+    for row in table.findAll("tr")[2:]:
+        # Get the txt from the bs4 object
+        rowContents = str(row.getText())
+        rowContents = rowContents.split("\n")
+        rowContents = [x for x in rowContents if len(x) > 0]
+
+        # Normal row with all 4 properties
+        if len(rowContents) > 3:
+            timeline = rowContents[0]
+            title = rowContents[1]
+            author = rowContents[2]
+            released = rowContents[3]
+            dateLen = len(released.split("-"))
+
+            # Fix weird dates missing month and day
+            while dateLen < 3:
+                released += '-01'
+                dateLen += 1
+
+        # Row is missing time line or author
+        elif len(rowContents) > 2:
+            # Fix weird dates
+            released = rowContents[-1]
+            dateLen = len(released.split("-"))
+            while dateLen < 3:
+                released += '-01'
+                dateLen += 1
+            #  Check if the author or timeline is the missing use the previous values
+            if 'BBY' in rowContents[0] or 'ABY' in rowContents[0]:
+                title = rowContents[1]
+                timeline = rowContents[0]
+            else:
+                title = rowContents[0]
+                author = rowContents[1]
+
+        # Both author and timeline are missing use the previous values
+        elif len(rowContents) > 1:
+            title = rowContents[0]
+            released = rowContents[-1]
+            released = str(released)
+            dateLen = len(released.split("-"))
+            while dateLen < 3:
+                released += '-01'
+                dateLen += 1
+
+        # Non title row means it a book append it to the list
+        if len(rowContents) > 1:
+            book = dict(timeline=timeline, title=title, author=author, released=released)
+            rows.append(book)
+    return rows
+
+
+def putToDb(data, mydb):
+    cursor = mydb.cursor()
+    sql = 'INSERT INTO StarWarsBooks (title, timeline,author,released) VALUES (%s, %s,%s,%s)'
+    # Format each row and put to the database
+    for x in data:
+        val = (x['title'], x['timeline'], x['author'], x['released'].strip())
+        cursor.execute(sql, val)
+    mydb.commit()
+
+
+def main():
+    # Get books from https://starwars.fandom.com/wiki/Timeline_of_Legends_books
+    data = defGetPage()
+    # Connect to mysql db
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        # Enter password here
+        password="<Password>",
+        # Enter Db here
+        database='<DB>'
+    )
+    putToDb(data, mydb)
+
+
+if __name__ == '__main__':
+    main()
