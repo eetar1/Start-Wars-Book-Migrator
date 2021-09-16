98ed26b0efe58e083edf51385897ee95a3662481
eetar1 <ekwan14@gmail.com>
2021-09-15 18:14:58 -0700

    update to work with new web format

--- migrator.py
+++ migrator.py
@@ -1,69 +1,54 @@
+import os
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
 import pymongo
+from dotenv import load_dotenv
+
+TITLE_IDX = 2
+YEAR_IDX = 0
+AUTHOR_IDX = 3
+RELEASE_IDX = 4
+TYPE_IDX = 1
+
+
+def extractFromTable(table, era):
+    eraBookList = []
+    for row in table.findAll("tr")[1:]:
+        # Get the txt from the bs4 object
+        rowContents = str(row.getText()).split('\n')
+        rowContents = [x for x in rowContents if len(x) > 0]
+        if len(rowContents) > 5:
+            rowContents.pop(3)
+
+        # Add number so that improper dates can be formatted
+        while len(rowContents[RELEASE_IDX].split("-")) < 3:
+            rowContents[RELEASE_IDX] += '-01'
+
+        released = datetime.strptime(rowContents[RELEASE_IDX], "%Y-%m-%d")
+
+        book = dict(title=rowContents[TITLE_IDX], author=rowContents[AUTHOR_IDX], year=rowContents[YEAR_IDX],
+                    type=rowContents[TYPE_IDX], era=era, releasedate=released, owned=False)
+        eraBookList.append(book)
+    return eraBookList
 
 
 def defGetPage():
     resp = requests.get('https://starwars.fandom.com/wiki/Timeline_of_Legends_books')
     soup = BeautifulSoup(resp.text, 'html.parser')
-    table = soup.findAll("table")[4]
+    tables = soup.findAll("table")[4:11]
+
+    # Pull the eras from the table of contents and remove the number
+    eras = soup.find(id='toc').find("ul").findAll("li")
+    eras = [x.getText()[2:] for x in eras]
+    eraCnt = 0
     rows = list()
 
-    # Initial inits these vars sometimes have to be used from previous rows
-    author = ''
-    timeline = ''
-    title = ''
-    released = ''
+    for table in tables:
+        rows += extractFromTable(table, eras[eraCnt])
+        eraCnt += 1
 
-    for row in table.findAll("tr")[2:]:
-        # Get the txt from the bs4 object
-        rowContents = str(row.getText())
-        rowContents = rowContents.split("\n")
-        rowContents = [x for x in rowContents if len(x) > 0]
-
-        # Normal row with all 4 properties
-        if len(rowContents) > 3:
-            timeline = rowContents[0]
-            title = rowContents[1]
-            author = rowContents[2]
-            released = rowContents[3]
-            dateLen = len(released.split("-"))
-
-            # Fix weird dates missing month and day
-            while dateLen < 3:
-                released += '-01'
-                dateLen += 1
-
-        # Row is missing time line or author
-        elif len(rowContents) > 2:
-            # Fix weird dates
-            released = rowContents[-1]
-            dateLen = len(released.split("-"))
-            while dateLen < 3:
-                released += '-01'
-                dateLen += 1
-            #  Check if the author or timeline is the missing use the previous values
-            if 'BBY' in rowContents[0] or 'ABY' in rowContents[0]:
-                title = rowContents[1]
-                timeline = rowContents[0]
-            else:
-                title = rowContents[0]
-                author = rowContents[1]
-
-        # Both author and timeline are missing use the previous values
-        elif len(rowContents) > 1:
-            title = rowContents[0]
-            released = rowContents[-1]
-            released = str(released)
-            dateLen = len(released.split("-"))
-            while dateLen < 3:
-                released += '-01'
-                dateLen += 1
-
-        # Non title row means it a book append it to the list
-        if len(rowContents) > 1:
-            book = dict(timeline=timeline, title=title, author=author, released=released)
-            rows.append(book)
     return rows
 
 
@@ -71,17 +56,17 @@ def putToDb(data, mycol):
     mycol.insert_many(data)
 
 
-
-def main():
+def main(URI):
     # Get books from https://starwars.fandom.com/wiki/Timeline_of_Legends_books
     data = defGetPage()
     # Connect to mysql db
-    myclient = pymongo.MongoClient("<URI>")
-
-    mydb = myclient["Cluster0"]
-    mycol = mydb["starwarsBooks"]
+    myclient = pymongo.MongoClient(URI)
+    mydb = myclient.Cluster0
+    mycol = mydb.starwarsbooks
     putToDb(data, mycol)
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    MONGO_URI = os.getenv('MONGO_URI')
+    main(MONGO_URI)
